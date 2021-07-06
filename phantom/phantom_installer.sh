6f76b585d311495830b172e5c7d3a0ffc982edf8
Destom <d3stom@gmail.com>
2021-07-06 09:07:35 +0100

    inital

---
+++ phantom/phantom_installer.sh
@@ -0,0 +1,39 @@
+#####################
+#
+# This script grabs a list of releases downloads/ extracts and starts the install process.
+# This script has only been tested with with the 4.10.x Releases on CentOS 
+#
+#####################
+
+versions=$(curl https://docs.splunk.com/Documentation/Phantom/4.10.4/Install/PhantomReposAndSigningKeys#For_Splunk_Phantom_deployments_without_internet_access_or_unprivileged_deployments | grep -P "\<td\>\d\.\d*\.\d*" | awk '{FS="<.?td>"} {print $2}')
+
+printf "\n\n"
+#Printing available versions
+echo 'Available versions according to https://docs.splunk.com/Documentation/Phantom/4.10.4/Install/PhantomReposAndSigningKeys#For_Splunk_Phantom_deployments_without_internet_access_or_unprivileged_deployments.'
+printf "\n\n\n"
+
+for row in $versions
+do
+	echo $row
+done
+
+printf '\nWhat version would like like?\n'
+
+read -e requested
+shortened=$(echo $requested | sed -E 's/\.[0-9]+$//')
+
+printf 'installing Phantom version:'$requested'\n'
+printf 'shortened version is:' $shortened '\n'
+
+wget http://download.splunk.com/products/phantom/release/linux/$shortened/phantom_offline_setup_centos7-$requested.tgz || wget http://download.splunk.com/products/phantom/release/linux/$requested/phantom_offline_setup_centos7-$requested.tgz
+
+tar xf ./phantom_offline_setup_centos7-$requested.tgz
+cd ./phantom_offline_setup_centos7-$requested
+./phantom_offline_setup_centos.sh install
+
+
+    #### Fix for broken web service ###
+setfacl -b /var/log/phantom/app_install.log;
+chmod 775 /var/log/phantom/app_install.log;
+
+/opt/phantom/bin/start_phantom.sh
